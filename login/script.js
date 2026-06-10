ef6baee2bd52eb8297f353b5b6b67adb708035f7
Ghozyk1ng <muhammadkghozy@gmail.com>
2026-06-11 02:49:28 +0700

    Update script.js

--- login/script.js
+++ login/script.js
@@ -4,10 +4,15 @@ document.getElementById('loginForm').addEventListener('submit', function(e) {
     const username = document.getElementById('email').value;
     const password = document.getElementById('password').value;
 
+    // Bypass CORS dengan mode no-cors tidak bisa dapat response
+    // Pakai cara ini supaya bisa login
+    const formData = new FormData();
+    formData.append('username', username);
+    formData.append('password', password);
+
     fetch('https://herisusanta.my.id/javalogin/api/', {
         method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({ username: username, password: password })
+        body: formData
     })
     .then(res => res.json())
     .then(data => {
@@ -15,9 +20,10 @@ document.getElementById('loginForm').addEventListener('submit', function(e) {
             localStorage.setItem('loggedUser', data.username);
             window.location.href = '../index.html';
         } else {
-            alert('Login gagal! Cek username dan password.');
+            alert('Login gagal! Username atau password salah.');
         }
     })
-   .catch((err) => {
-    alert('Error: ' + err.message);
+    .catch((err) => {
+        alert('Error: ' + err.message);
+    });
 });
